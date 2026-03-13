851e7b94179e107da344786ca1a37dcf95b917c4
Dre Johnson <dre@phreshr.com>
2026-03-13 09:55:08 -0400

    chore: Delete the `surrealdb-better-auth` library's source code, tests, and examples, and update associated configuration and documentation files.

--- tests/browser/global-setup.ts
+++
@@ -1,9 +0,0 @@
-import { startTestServer } from "../__helpers__/server";
-
-export default async function globalSetup() {
-  const server = await startTestServer({ port: 3002 });
-
-  return async () => {
-    await server.stop();
-  };
-}
